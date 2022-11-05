725d8c431e6d7e35872098a3d972dde4cf1878a8
PasinduPerera10 <pasinduperera9910@gmail.com>
2022-11-06 03:53:37 +0530

    commit 3

--- mobile_frontend/src/navigation/AppNav.jsx
+++ mobile_frontend/src/navigation/AppNav.jsx
@@ -3,20 +3,13 @@ import Login from "../components/authentication/Login";
 import AuthContext from "../context/UserContext";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./BottomNav";
-import AddAction from "../components/IncidentManagement/addAction";
-import UpdateAction from "../components/IncidentManagement/updateAction";
-import AddIncident from "../components/IncidentManagement/AddIncident";
-import ViewIncident from "../components/IncidentManagement/viewIncident";
-import ViewAllIncidents from "../components/IncidentManagement/viewAllIncidents";
-import RequestList from "../components/RequestManagement/RequestList";
-import UpdateIncident from "../components/IncidentManagement/UpdateIncident";
 
 function AppNav() {
   const { userType } = useContext(AuthContext);
 
   return (
     <>
-      {userType === null && <ViewAllIncidents />}
+      {userType === null && <Login />}
       {userType === "Site Manager" && (
         <NavigationContainer>
           <Tabs />
