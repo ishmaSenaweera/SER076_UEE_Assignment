9e20394e946b6ff5c979281595600d8ce93a3d07
ishmaSenaweera <86140992+ishmaSenaweera@users.noreply.github.com>
2022-11-11 02:23:57 +0530

    Ride Request Views

--- mobile_frontend/src/components/RequestManagement/RequestList.jsx
+++ mobile_frontend/src/components/RequestManagement/RequestList.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Card, Icon } from "@rneui/themed";
 import {
+  Image,
   ScrollView,
   StyleSheet,
   Text,
@@ -11,10 +12,13 @@ import axios from "axios";
 import SearchBar from "react-native-dynamic-search-bar";
 import { useIsFocused } from "@react-navigation/native";
 import { BASE_URL } from "../constants/Url.json";
+import AuthContext from "../../context/UserContext";
 
 export default function RequestList({ navigation }) {
   const [requestList, setRequestList] = useState([]);
   const isFocused = useIsFocused();
+  const [searchTerm, setSearchTerm] = useState("");
+  const { userName } = useContext(AuthContext);
 
   async function getRequestData() {
     try {
@@ -35,14 +39,14 @@ export default function RequestList({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.TextTitle1}>Hi</Text>
+      <Text style={styles.TextTitle1}>Hi {userName},</Text>
       <View style={styles.row}>
-        <Text style={styles.TextTitle2}>Ride Request</Text>
+        <Text style={styles.TextTitle2}>Ride Requests</Text>
         <TouchableOpacity onPress={() => navigation.navigate("AddRequest", {})}>
           <Icon
             name="add"
             color="#000000"
-            iconStyle={{ marginLeft: 60, fontSize: 30 }}
+            iconStyle={{ marginLeft: 60, fontSize: 40 }}
           />
         </TouchableOpacity>
       </View>
@@ -51,44 +55,73 @@ export default function RequestList({ navigation }) {
         <SearchBar
           type="search"
           placeholder="Search By Location"
-          // onChangeText={(order) => {
-          //   searchOrder(order);
-          // }}
-          // onClearPress={getAllOrder}
+          onChangeText={(location) => {
+            setSearchTerm(location);
+          }}
+          onClearPress={getRequestData}
         />
       </View>
       <ScrollView style={{ height: "58%", marginBottom: 10 }}>
-        {requestList.map((element, index) => {
-          return (
-            <TouchableOpacity
-              onPress={() => navigation.navigate("ViewRequestInfo", {})}
-            >
-              <View
+        {requestList
+          .filter((element) => {
+            if (searchTerm === "") {
+              return element;
+            } else if (
+              element.locationTo
+                .toLowerCase()
+                .includes(searchTerm.toLowerCase())
+            ) {
+              return element;
+            } else {
+              return false;
+            }
+          })
+          .map((element, index) => {
+            // if(element.length === 0){
+            //   <Text style={styles.errorMessage}>No Result Found</Text>
+            // }
+            return (
+              <TouchableOpacity
+                onPress={() => navigation.navigate("ViewRequestInfo", {})}
                 key={index}
-                style={{
-                  borderWidth: 1,
-                  height: 100,
-                  margin: 15,
-                  backgroundColor: "#DFD8D7",
-                  borderColor: "#DFD8D7",
-                  borderRadius: 15,
-                }}
               >
-                <View style={{ padding: 10 }}>
-                  <Text style={styles.text1}>
-                    Name :{" "}
-                    {element.passenger.firstName +
-                      " " +
-                      element.passenger.lastName}
-                  </Text>
-                  <Text style={styles.text2}>
-                    Location : {element.locationTo}
-                  </Text>
+                <View
+                  style={{
+                    borderWidth: 1,
+                    height: 100,
+                    margin: 15,
+                    backgroundColor: "#DFD8D7",
+                    borderColor: "#DFD8D7",
+                    borderRadius: 15,
+                    elevation: 20,
+                  }}
+                >
+                  <View style={styles.row1}>
+                    <Image
+                      source={{
+                        uri: `https://xsgames.co/randomusers/assets/avatars/male/${index}.jpg`,
+                      }}
+                      style={{
+                        width: 80,
+                        height: 80,
+                        resizeMode: "contain",
+                        margin: 8,
+                      }}
+                    />
+
+                    <View>
+                      <Text style={styles.text1}>
+                        {element.passenger.firstName +
+                          " " +
+                          element.passenger.lastName}
+                      </Text>
+                      <Text style={styles.text2}>{element.locationTo}</Text>
+                    </View>
+                  </View>
                 </View>
-              </View>
-            </TouchableOpacity>
-          );
-        })}
+              </TouchableOpacity>
+            );
+          })}
       </ScrollView>
     </View>
   );
@@ -112,9 +145,9 @@ const styles = StyleSheet.create({
   },
   icon: { marginTop: 10, marginRight: 20, fontSize: 35 },
   TextTitle1: {
-    padding: 0,
-    fontSize: 30,
-    marginLeft: 50,
+    marginTop: 20,
+    fontSize: 20,
+    marginLeft: 45,
   },
   TextTitle2: {
     padding: 0,
@@ -124,8 +157,19 @@ const styles = StyleSheet.create({
   text1: {
     fontWeight: "bold",
     fontSize: 20,
+    textAlign: "center",
   },
   text2: {
     fontSize: 15,
+    textAlign: "center",
+  },
+  row1: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "center",
+  },
+  errorMessage: {
+    color: "black",
+    textAlign: "center",
   },
 });
