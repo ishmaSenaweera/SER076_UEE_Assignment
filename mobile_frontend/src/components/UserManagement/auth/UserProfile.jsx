1369d807ea12f967da4a3f07a574ff0d9e4caa1e
sathmini <86968596+sathmini@users.noreply.github.com>
2022-11-12 03:29:32 +0530

    changing code

--- mobile_frontend/src/components/UserManagement/auth/UserProfile.jsx
+++ mobile_frontend/src/components/UserManagement/auth/UserProfile.jsx
@@ -1,77 +1,104 @@
-import { StatusBar } from "expo-status-bar";
-import { useContext, useState,useEffect} from "react";
+import React, { useState } from "react";
+import { Card, Icon } from "@rneui/themed";
 import {
+  Button,
   StyleSheet,
   Text,
-  View,
   TextInput,
   TouchableOpacity,
-  ScrollView,
+  View,
 } from "react-native";
-import axios from "axios";
 
+export default function UserProfile({ navigation }) {
 
+  
+  return (
+    <View>
+      <View style={styles.row}>
+        <Text style={styles.TextTitle1}>User Info</Text>
+      </View>
 
+      <Card.Divider color="black" style={{ height: 4 }} />
 
-      const styles = StyleSheet.create({
-      container: {
-      marginTop: 70,
-      marginHorizontal: 20,
-      elevation: 20,
-      borderRadius: 10,
-      backgroundColor: "#ebd9fa",
-      alignItems: "center",
-      justifyContent: "center",
-    },
+      <View style={styles.container1}>
+        <Text style={styles.TextInput}>First Name : </Text>
+        <Text style={styles.TextInput}>Last Name : </Text>
+        <Text style={styles.TextInput}>E-Mail : </Text>
+        <Text style={styles.TextInput}>Phone Number: </Text>
+        <Text style={styles.TextInput}>Date Of Birth : </Text>
+        <Text style={styles.TextInput}>User Type : </Text>
+
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.updateBtn}>
+            <Text style={styles.reqText}>Update</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.deleteBtn}>
+            <Text style={styles.reqText}>Delete</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  icon: { fontSize: 35 },
+  TextTitle1: {
+    marginTop: 10,
+    fontSize: 30,
+  },
+  container1: {
+    backgroundColor: "#D5BEFF",
+    margin: 10,
+    borderWidth: 1,
+    borderColor: "#D5BEFF",
+    borderRadius: 25,
+    height: "80%",
+  },
+  TextInput: {
+    height: 50,
+    padding: 10,
+    borderWidth: 5,
+    marginTop: 0,
+    marginLeft: 10,
+    marginRight: 10,
+    borderRadius: 10,
+    borderColor: "#8B51F5",
+    marginTop: 10,
+    marginBottom: 10,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  row: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "center",
+  },
+  updateBtn: {
+    width: "25%",
+    borderRadius: 25,
+    marginTop: 25,
+    marginLeft: 20,
+    marginBottom: 20,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#8B51F5",
+  },
   
-    header: {
-      fontSize: 30,
-      fontWeight: "bold",
-      alignItems: "center",
-    },
-  
-    bar: {
-      flex: 1,
-      height: 3,
-      backgroundColor: "grey",
-      width: "20%",
-      marginBottom: 20,
-      marginLeft: 20,
-      marginRight: 20,
-    },
-  
-    header2: {
-      fontSize: 20,
-      fontWeight: "bold",
-      marginBottom: 10,
-      alignItems: "center",
-    },
-  
-    inputView: {
-      backgroundColor: "white",
-      elevation: 20,
-      borderColor: "#f2bc57",
-      borderRadius: 10,
-      width: "80%",
-      height: 45,
-      marginBottom: 20,
-    },
-  
-    TextInput: {
-      height: 20,
-      flex: 1,
-      padding: 10,
-      marginLeft: 20,
-    },
-  
-    loginBtn: {
-      width: "70%",
-      borderRadius: 10,
-      marginTop: 10,
-      marginBottom: 20,
-      height: 50,
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: "#bb8de0",
-    },
-  });
+  deleteBtn: {
+    width: "25%",
+    borderRadius: 25,
+    marginTop: 25,
+    marginLeft: 25,
+    marginBottom: 20,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#F55151",
+  },
+  reqText: {
+    color: "white",
+    fontSize: 20,
+  },
+});
